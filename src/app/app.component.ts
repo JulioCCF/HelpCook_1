a8c5e5932a9264e0b97782d9b4607d44816dfbf3
Hugo A <hugoalonradev@gmail.com>
2023-03-06 02:35:23 +0100

    Añadidos estilos de Noah. Redirección de botones de registrar e iniciar sesión. Bara de navegación visible con la plantilla de Noah. Carpeta img creada para añadir logo e iconos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelpCook';
+  opened = false;
+  expanded = false;
+
+  toggleSidenav() {
+    this.expanded = !this.expanded;
+  }
 }
