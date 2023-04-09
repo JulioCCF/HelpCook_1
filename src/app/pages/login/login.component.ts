f2c4048d6056c0017c57732eac311befe123d4e2
Hugo A <hugoalonradev@gmail.com>
2023-04-09 21:28:45 +0200

    required en formulario

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  email: string;
+  password: string;
+
+  constructor(private router: Router) {
+
+  }
+  ngOnInit(): void {
+    
+  }
+
+  // Recibirá los datos del formulario mediante NgForm
+  login(form: NgForm) {
+    // Rescatamos los campos que vienen del formulario
+    this.email = form.value.email;
+    this.password = form.value.password;
+  }
+  
 }
