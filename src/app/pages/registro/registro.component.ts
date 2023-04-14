8586919725688b3afcd1db9957578932f767a566
noahjimab6 <noahjimab6@gmail.com>
2023-04-15 00:41:20 +0200

    registroService añadido

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
+
+export class RegistroComponent{
+  
+  User = new FormGroup({
+    nombre : new FormControl('', Validators.required), 
+    apellidos : new FormControl('', Validators.required),
+    nick : new FormControl('', Validators.required),
+    email : new FormControl('', Validators.required),
+    contrasenia : new FormControl('', Validators.required),
+    foto : new FormControl('', Validators.required)
+  })
+
+  constructor(private fb: FormBuilder) { }
+
+  saveUser(form: any){
+    console.log(this.User.value)
+  }
 
 }
