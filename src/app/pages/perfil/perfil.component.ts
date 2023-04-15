0e9dca016146af927ccfcb23897d55195f0016c5
Hugo A <hugoalonradev@gmail.com>
2023-04-16 01:52:13 +0200

    interfaz perfil y cambio minusmayus recetas.module

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PerfilService } from './perfil-service.service';
+import { Usuarios } from 'src/app/Usuarios.model';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit{
 
+  nombre: string;
+  apellido: string;
+  nick: string;
+  email: string;
+  contrasenia: string;
+  contraseniaRep: string;
+  foto: string;
+  recetaGuardada: string;
+  recetaSubida: string;
+
+  constructor(private perfilServicio: PerfilService) {
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  obtenerUsuario() {
+    let usuario = new Usuarios(this.nick, this.contrasenia, this.nombre, this.apellido, this.email, this.foto);
+    let id: number;
+
+    this.perfilServicio.getUser(id).subscribe(response => {
+      
+    })
+  }
 }
